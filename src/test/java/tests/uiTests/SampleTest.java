66fc7bcdfe595e51e5ba5958859b031a755409bb
hazephoenix <Hp180790holyphoenixgit>
2019-09-28 23:20:20 +0700

    uiTestsScratches2

--- src/test/java/tests/uiTests/SampleTest.java
+++ src/test/java/tests/uiTests/SampleTest.java
@@ -2,13 +2,13 @@ package tests.uiTests;
 
 import BaseClasses.uii.uiTestBase.TestBase;
 import com.codeborne.selenide.CollectionCondition;
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
 import org.junit.Test;
 import org.openqa.selenium.By;
 
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 import static com.codeborne.selenide.Condition.exist;
 import static com.codeborne.selenide.Condition.visible;
@@ -21,14 +21,21 @@ public class SampleTest extends TestBase {
     @Test
     public void sample() {
         List<String> menus = Arrays.asList("КАТАЛОГ ПОТОЛКОВ", "ОСВЕЩЕНИЕ", "ФОТОГАЛЕРЕЯ","ПРАЙС-ЛИСТ","ПОЛЕЗНЫЕ СТАТЬИ", "ВАШИ ВОПРОСЫ", "КОНТАКТЫ");
-        List<String> catalog_items = Arrays.asList("Одноуровневые потолки", "Двухуровневые потолки", "Тканевые потолки", "\"Парящие потолки\"", "Криволинейная спайка потолка", "Фотопечать");
+        List<String> items = Arrays.asList("Одноуровневые потолки", "Двухуровневые потолки", "Тканевые потолки", "\"Парящие потолки\"", "Криволинейная спайка потолка", "Фотопечать");
+        List<String> types = Arrays.asList("Квартиры, частные дома", "Офисные помещения");
+        List <Object> picturesUrls = new ArrayList<>();
         open("/");
         $$(".topmenu.nav>li").
                 shouldHaveSize(7).
                 shouldHave(CollectionCondition.exactTexts(menus));
         $(By.xpath("(//a[text()='Фотогалерея'])[1]")).click();
         $("#zag_2").should(exist);
-        $$("#catalog_cat .cat_zag").shouldHaveSize(6).shouldHave(CollectionCondition.exactTexts(catalog_items));
+        $$("#catalog_cat .cat_zag").shouldHaveSize(6).shouldHave(CollectionCondition.exactTexts(items));
         $(By.xpath("//*[contains(text(),'ТИП ПОМЕЩЕНИЯ')]")).click();
+        $$("#catalog_cat>div .cat_zag").shouldHaveSize(2).shouldHave(CollectionCondition.exactTexts(types));
+        $(By.xpath("//*[contains(text(),'ЛЮСТРЫ И СВЕТИЛЬНИКИ')]")).click();
+        $$("#catalog_cat a").forEach(element->picturesUrls.add(element.getAttribute("href")));
+        System.out.println(picturesUrls);
+
     }
 }
