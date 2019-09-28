400e6f6c167a827dc010c80e2cf62cd50c2848f4
hazephoenix <Hp180790holyphoenixgit>
2019-09-28 21:51:59 +0700

    uiTestsScratch

--- src/test/java/tests/uiTests/SampleTest.java
+++ src/test/java/tests/uiTests/SampleTest.java
@@ -10,7 +10,9 @@ import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
+import static com.codeborne.selenide.Condition.exist;
 import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
@@ -19,10 +21,14 @@ public class SampleTest extends TestBase {
     @Test
     public void sample() {
         List<String> menus = Arrays.asList("КАТАЛОГ ПОТОЛКОВ", "ОСВЕЩЕНИЕ", "ФОТОГАЛЕРЕЯ","ПРАЙС-ЛИСТ","ПОЛЕЗНЫЕ СТАТЬИ", "ВАШИ ВОПРОСЫ", "КОНТАКТЫ");
+        List<String> catalog_items = Arrays.asList("Одноуровневые потолки", "Двухуровневые потолки", "Тканевые потолки", "\"Парящие потолки\"", "Криволинейная спайка потолка", "Фотопечать");
         open("/");
-        Selenide.$$(".topmenu.nav>li").
+        $$(".topmenu.nav>li").
                 shouldHaveSize(7).
                 shouldHave(CollectionCondition.exactTexts(menus));
-        Selenide.$(By.xpath("(//a[text()='Фотогалерея'])[1]")).click();
+        $(By.xpath("(//a[text()='Фотогалерея'])[1]")).click();
+        $("#zag_2").should(exist);
+        $$("#catalog_cat .cat_zag").shouldHaveSize(6).shouldHave(CollectionCondition.exactTexts(catalog_items));
+        $(By.xpath("//*[contains(text(),'ТИП ПОМЕЩЕНИЯ')]")).click();
     }
 }
